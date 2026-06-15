6fd1f0057ad1554cfc03cb90380dfbfdaa89e398
nastazyja <anastasia.wirt@googlemail.com>
2026-06-15 12:38:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,44 @@
-self.addEventListener('install', () => {
+const CACHE_NAME = "spielplatzatlas-v4";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./images/icon-192.png",
+  "./images/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
+});
+
+// ACTIVATE (wichtig: alte Caches löschen!)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
 });
